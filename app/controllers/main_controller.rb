2670dc7522d142eb9f2677ab8e7b23b165417d44
Dmitriy Malakhov <d.malakhov.v@gmail.com>
2023-12-02 18:00:10 +0300

    Added initial login logic

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,4 +1,10 @@
 class MainController < ApplicationController
   def index
   end
+
+  def secret
+    unless current_user
+      render plain: '401 Unauthorized', status: :unauthorized
+    end
+  end
 end
