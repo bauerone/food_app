41d6efd76c33f61b4fc347311103985f6b7025c1
Dmitriy Malakhov <d.malakhov.v@gmail.com>
2023-12-11 19:49:26 +0300

    Added user logic for weight and preferences

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,17 @@ class User < ApplicationRecord
 
   has_one :food_preference
   has_many :nutrition_plans
+  has_many :weight_measures
+
+  def current_weight
+    weight_measures.last&.weight
+  end
+
+  def favourite_products
+    food_preference.products.where('food_preference_products.status = ?', FoodPreferenceProduct.statuses[:positive])
+  end
+
+  def blocked_products
+    food_preference.products.where('food_preference_products.status = ?', FoodPreferenceProduct.statuses[:negative])
+  end
 end
